0ce037674d44b90ee747136aa53028b536424f73
Vedant Damedhar <damedharvedant1.com>
2024-11-26 23:06:08 +0530

    Change somthing in ui

--- models/book.js
+++ models/book.js
@@ -1,12 +1,5 @@
 const mongoose = require('mongoose');
 
-// Single connection to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/databaseName', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Database connected successfully'))
-  .catch(err => console.log('Database connection error:', err));
 
 // Define the book schema
 const bookSchema = new mongoose.Schema({
